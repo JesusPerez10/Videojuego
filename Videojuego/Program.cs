a2e30ea1a638134a0885ac9f6e71a8d92439f3ef
Jesus <Jesus@DESKTOP-JN5LAKQ>
2024-04-20 10:51:08 -0600

    Agregar archivos de proyecto.

---
+++ Videojuego/Program.cs
@@ -0,0 +1,10 @@
+﻿using Videojuego.Clase_base;
+
+JuegoAccion ac = new JuegoAccion();
+ac.armas = true;
+ac.titulo = "Call of Duty";
+ac.AnioLanzamiento = "2017";
+ac.calificacion = "10";
+ac.jugando = true;
+string resultado = ac.mostrardetallesAccion();
+Console.WriteLine(resultado);
---
+++ Videojuego/clases/JuegoAccion.cs
@@ -0,0 +1,14 @@
+﻿using Videojuego.Clase_base;
+
+namespace Videojuego.clases
+{
+    internal class JuegoAccion : Videojuegos
+    {public string armas { get; set; }
+        public string mostrardetallesAccion()
+        {
+            mostrardetallesAccion();
+            return $"Tiene armas:{armas}";
+        }
+
+    }
+}
